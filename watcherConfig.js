abbfcbcfe7350d0cef388d428b9126c8ad8e2631
Markus Kobisch <kob@localhost.localdomain>
2015-12-21 21:16:44 +0100

    Execute custom commands on start and on change

--- watcherConfig.js
+++ watcherConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // The directory which will be watched. If falsy, the parent directory of this module will be watched.
+    // The directory which will be watched. If falsy, the current working directory will be watched.
     directory: '.',
     // ignore can be a string, regex, function or an array containing any of them. Has to be anymatch compatible, see https://github.com/es128/anymatch
     ignore: [
@@ -7,8 +7,12 @@ module.exports = {
         /\.git/
     ],
     delay: 1000,
-    // The command to be executed to watch sass files. Leave empty to don't use sass.
-    sass: 'sass --watch .:.',
-    // The command to be executed when a change occurs. Leave empty to don't use rsync.
-    rsync: 'rsync -aP --delete source/ target'
+    commandsOnStart: [{
+        name: 'sass',
+        command: 'sass --watch .:.'
+    }],
+    commandsOnChange: [{
+        name: 'rsync',
+        command: 'rsync -aP --delete source/ target'
+    }]
 };
