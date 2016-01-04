bf1df3932e83d112e9b67e7f3bcfbe154bd26f42
Markus Kobisch <kob@localhost.localdomain>
2016-01-04 14:46:44 +0100

    Re-structured example config file

--- watcherConfig.js
+++ watcherConfig.js
@@ -1,16 +1,22 @@
-module.exports = {
+var rsync = { // command can be a string or a callback function returning a string. The callback gets the arguments »event, file«.
+    name: 'rsync',
+    command: 'rsync -aP --delete source/ target'
+};
+
+var sass = {
+    name: 'sass',
+    command: 'sass --watch .:.'
+};
+
+var config = {
     directory: '.', // The directory which will be watched. If falsy, the current working directory will be watched.
     ignore: [ // ignore can be a string, regex, function or an array containing any of them. Has to be anymatch compatible, see https://github.com/es128/anymatch
         /node_modules/,
         /\.git/
     ],
     delay: 1000,
-    commandsOnStart: [{
-        name: 'sass',
-        command: 'sass --watch .:.'
-    }],
-    commandsOnChange: [{ // command can be a string or a callback function returning a string. The callback gets the arguments »event, file«.
-        name: 'rsync',
-        command: 'rsync -aP --delete source/ target'
-    }]
+    commandsOnStart: [],
+    commandsOnChange: []
 };
+
+module.exports = config;
