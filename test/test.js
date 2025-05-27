d32e6a3cefb924b4a7f774c64c1b6b95fa59fc32
Markus Kobisch <maevic@users.noreply.github.com>
2025-05-27 10:18:03 +0200

    Format files

--- test/test.js
+++ test/test.js
@@ -5,23 +5,23 @@ process.env.testing = true;
 var expect = require('chai').expect;
 var watcher = require('../lib/node-watcher').test;
 
-describe('Command parsing', function() {
-	describe('parseCommand(pm2 logs)', function() {
+describe('Command parsing', function () {
+	describe('parseCommand(pm2 logs)', function () {
 		var parsedCommand = watcher.parseCommand('pm2 logs');
 
-		it('should return an object', function() {
+		it('should return an object', function () {
 			expect(parsedCommand).is.an('object');
 		});
 
-		it('the command string should be equal to »pm2«', function() {
+		it('the command string should be equal to »pm2«', function () {
 			expect(parsedCommand.command).is.a('string').and.equal('pm2');
 		});
 
-		it('the arguments should be [ \'logs\' ]', function() {
+		it("the arguments should be [ 'logs' ]", function () {
 			expect(parsedCommand.arguments).is.an('array').which.have.all.members(['logs']);
 		});
 
-		it('the remainingString should equal »«', function() {
+		it('the remainingString should equal »«', function () {
 			expect(parsedCommand.remainingString).is.a('string').which.equal('');
 		});
 	});
